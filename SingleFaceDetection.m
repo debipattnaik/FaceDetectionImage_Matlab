ed5f4c09531e18db35e5a996f4af256c9ab858ea
debipattnaik <debipattnaik@users.noreply.github.com>
2020-06-01 13:40:32 +0100

    Add files via upload

--- SingleFaceDetection.m
+++ SingleFaceDetection.m
@@ -1,7 +1,6 @@
 % detecting single face in an image using Viola-Jones Algorithm
-% DPattnaik
 clear all
-image=imread('CVRaman.jpg');
+image=imread('MoonMission3.jpg');
 [width,height]=size(image);
 if width >320
     image=imresize(image,[320 NaN]);
@@ -14,5 +13,19 @@ imshow(image); hold on
 for i = 1:size(LocationFace,1)
     rectangle('Position',LocationFace(i,:),'LineWidth',1,'LineStyle','-','EdgeColor','r');
 end
+title('Face Detection');
+hold off
+% For images with multiple faces and more using upper body identification
+
+DetectBody=vision.CascadeObjectDetector('UpperBody');
+DetectBody.MinSize=[70,70];
+DetectBody.ScaleFactor=1.05;
+LocationBody=step(DetectBody,image); %Bounding Box values based on number of objects
+ 
+figure(2),
+imshow(image); hold on
+ for i = 1:size(LocationBody,1)
+    rectangle('Position',LocationBody(i,:),'LineWidth',1,'LineStyle','-','EdgeColor','g');
+ end
 title('Face Detection');
 hold off
