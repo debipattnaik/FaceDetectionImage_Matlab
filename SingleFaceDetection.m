438ce333cb82937b9464e7cc77f91c4540a87b31
debipattnaik <debipattnaik@users.noreply.github.com>
2020-06-01 11:18:12 +0100

    Add files via upload

---
+++ SingleFaceDetection.m
@@ -0,0 +1,17 @@
+% detecting single face in an image using Viola-Jones Algorithm
+clear all
+image=imread('CVRaman.jpg');
+[width,height]=size(image);
+if width >320
+    image=imresize(image,[320 NaN]);
+end
+DetectFace = vision.CascadeObjectDetector();
+LocationFace=step(DetectFace,image); %Bounding Box values based on number of objects
+
+figure,
+imshow(image); hold on
+for i = 1:size(LocationFace,1)
+    rectangle('Position',LocationFace(i,:),'LineWidth',1,'LineStyle','-','EdgeColor','r');
+end
+title('Face Detection');
+hold off
